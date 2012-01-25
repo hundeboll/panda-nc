aaddcd73dbe2ff596f0cfeeac0f30c336b1a1d8d
Martin Hundebøll <martin@hundeboll.net>
2012-01-25 11:02:14 +0100

    Update riddler to new git infrastructure

--- riddler.sh
+++ riddler.sh
@@ -1,37 +1,46 @@
 #!/bin/sh
 
 # Check arguments
-USAGE="Usage: ${0} {start|stop} CONFIG"
-if [ $# -ne 2 ]; then
-  echo ${USAGE}
+USAGE="Usage: $0 {start|stop} [CONFIG]"
+if [ $# -lt 1 ]; then
+  echo $USAGE
+fi
+
+if [ $# -gt 1 ]; then
+  CONFIG=$2
+else
+  CONFIG=$(dirname $0)/nc.conf
 fi
 
 # Source config
-source ${2}
+source $CONFIG
 
-if [ ${1} = "start" ]; then
+start_riddler() {
   # Update git repository
-  cd ${RD_PATH}
+  path=$(pwd)
+  cd $RD_PATH
   git fetch origin
   git reset --hard origin
-
-  if [ -e ${RD_PID} ]; then
-    echo Killing running instance
-    kill $(cat ${RD_PID}) &> /dev/null
-    sleep 1
-  fi
+  cd $path
 
   # Start riddler node
-  ./node.py --wifi_iface ${WIRELESS_IFACE} --mesh_host ${BAT_IP%/*} | tee ${RD_LOG} &
+  ./node.py --wifi_iface $WIRELESS_IFACE --mesh_host ${BAT_IP%/*} | tee $RD_LOG &
   if [ $? -eq 0 ]; then
-    echo $! > ${RD_PID}
+    echo $! > $RD_PID
   fi
+}
 
-elif [ ${1} = "stop" ]; then
-  if [ -e ${RD_PID} ]; then
-    kill $(cat ${RD_PID}) &> /dev/null
-    rm ${RD_PID}
-  else
-    echo Riddler node does not seem to be running.
+stop_riddler() {
+  if [ -e $RD_PID ]; then
+    kill $(cat $RD_PID) &> /dev/null
+    rm $RD_PID
   fi
+}
+
+if [ $1 = "start" ]; then
+  stop_riddler
+  start_riddler
+
+elif [ $1 = "stop" ]; then
+  stop_riddler
 fi
